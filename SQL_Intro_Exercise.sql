228593483f42bdc391cdd6165c0b1b5b88f75cd9
johndward01 <johndward01@gmail.com>
2023-02-20 09:24:53 -0600

    init

---
+++ SQL_Intro_Exercise.sql
@@ -0,0 +1,27 @@
+-- find all products
+SELECT *
+FROM products
+
+-- find all products that cost $1400
+
+-- find all products that cost $11.99 or $13.99
+
+-- find all products that do NOT cost 11.99 - using NOT
+
+-- find all products and sort them by price from greatest to least
+
+-- find all employees who don't have a middle initial
+
+-- find distinct product prices
+
+-- find all employees whose first name starts with the letter ‘j’
+
+-- find all Macbooks
+
+-- find all products that are on sale
+
+-- find the average price of all products
+
+-- find all Geek Squad employees who don't have a middle initial
+
+-- find all products from the products table whose stock level is in the range -- of 500 to 1200. Order by Price from least to greatest. Hint: Use the between keyword
